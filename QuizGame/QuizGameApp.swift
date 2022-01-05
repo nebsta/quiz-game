ba7a55fc5b4dd9341661578be4e4d5c69d80536a
Benjamin Wallis <ben.nat.wallis@gmail.com>
2022-01-05 10:48:51 +0100

    first commit

---
+++ QuizGame/QuizGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGameApp.swift
+//  QuizGame
+//
+//  Created by Benjamin Wallis on 05/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
